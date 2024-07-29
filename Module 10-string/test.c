d494f7248982d734c97bf05b35f0586e8ea7f77e
samiulsani <samiulslm5@gmail.com>
2024-07-29 11:44:03 +0600

    push test

---
+++ Module 10-string/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf(
+     "git push test"
+    );//This file for testing push the folder under the main repository. To do this we need to put this instruction :git add . git commit -m "message" git push origin main(if do from main repository do git push only)
+
+    return 0;
+}
